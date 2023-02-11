ff22371308c6dcba9e68b25c5d2150cb0a9c85d3
Joey Anuff <joeyanuff@gmail.com>
2023-02-10 18:17:37 -0800

    removed response headline

--- pages/index.tsx
+++ pages/index.tsx
@@ -155,11 +155,11 @@ const Home: NextPage = () => {
             <motion.div className="space-y-10 my-10">
               {advice && (
                 <>
-                  <div>
+                  {/* <div>
                     <h2 className="sm:text-4xl text-3xl font-bold text-slate-900 mx-auto">
                       Your generated advice
                     </h2>
-                  </div>
+                  </div> */}
                   <div className="space-y-8 flex flex-col items-center justify-center max-w-xl mx-auto">
                     <div
                       className="bg-white rounded-xl shadow-md p-4 hover:bg-gray-100 transition cursor-copy border"
